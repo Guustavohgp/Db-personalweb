02632ab22ac06743fc944a2f965dae689b8db9f6
Guustavohgp <gustavo.henriquegpires@gmail.com>
2024-10-01 15:34:41 -0300

    create code repository

--- db/src/main/java/com/dbpersonal/db/repository/PessoaRepository.java
+++ db/src/main/java/com/dbpersonal/db/repository/PessoaRepository.java
@@ -1,5 +1,8 @@
 package com.dbpersonal.db.repository;
 
-public class PessoaRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.dbpersonal.db.entities.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    // Métodos adicionais podem ser adicionados se necessário
 }
