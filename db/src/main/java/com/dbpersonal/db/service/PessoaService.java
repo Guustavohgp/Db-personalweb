5f278a85ee9522b931418b4cb7bc2b4e008ca9ce
Guustavohgp <gustavo.henriquegpires@gmail.com>
2024-10-01 15:42:00 -0300

    att ajustes nos espaços

--- db/src/main/java/com/dbpersonal/db/service/PessoaService.java
+++ db/src/main/java/com/dbpersonal/db/service/PessoaService.java
@@ -2,10 +2,8 @@ package com.dbpersonal.db.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.dbpersonal.db.entities.Pessoa;
 import com.dbpersonal.db.repository.PessoaRepository;
-
 import jakarta.annotation.PostConstruct;
 
 @Service
@@ -14,6 +12,7 @@ public class PessoaService {
     @Autowired
     private PessoaRepository pessoaRepository;
 
+    //
     @PostConstruct
     public void salvarPessoasIniciais() {
         Pessoa pessoa1 = new Pessoa("João", 28, "Masculino", "Ectomorfo", "Intermediário");
