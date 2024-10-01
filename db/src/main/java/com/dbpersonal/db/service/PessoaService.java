04266aa1cf556ffb9b3d914113253981cd7deb82
Guustavohgp <gustavo.henriquegpires@gmail.com>
2024-10-01 15:35:31 -0300

    create code service

--- db/src/main/java/com/dbpersonal/db/service/PessoaService.java
+++ db/src/main/java/com/dbpersonal/db/service/PessoaService.java
@@ -1,5 +1,27 @@
 package com.dbpersonal.db.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dbpersonal.db.entities.Pessoa;
+import com.dbpersonal.db.repository.PessoaRepository;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
 public class PessoaService {
-    
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @PostConstruct
+    public void salvarPessoasIniciais() {
+        Pessoa pessoa1 = new Pessoa("João", 28, "Masculino", "Atlético", "Intermediário");
+        Pessoa pessoa2 = new Pessoa("Maria", 22, "Feminino", "Endomorfo", "Iniciante");
+
+        pessoaRepository.save(pessoa1);
+        pessoaRepository.save(pessoa2);
+
+        System.out.println("Pessoas salvas no banco de dados com sucesso!");
+    }
 }
